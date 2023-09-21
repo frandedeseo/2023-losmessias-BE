15e11e4c83bae2f2150cbe2883751508a882aaef
AugusR7 <augusramseyer197@gmail.com>
2023-09-20 22:45:35 -0300

    Project creation (BE)

---
+++ src/main/java/com/losmessias/leherer/LehererApplication.java
@@ -0,0 +1,13 @@
+package com.losmessias.leherer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LehererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LehererApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/losmessias/leherer/LehererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losmessias.leherer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LehererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
